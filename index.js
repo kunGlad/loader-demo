97ab4ba4e89c4816e516836f5da79f49ba4be0a9
zhaokun <1613430161@qq.com>
2024-01-05 21:02:47 +0800

    自定义loader(给每个打包文件前缀增加标识)

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const Comp = function (content) {
-  // 这个content就是传过来的ast语法树
-  console.log("走到demo-loader了");
-  return content.replace(/console\.log/g, "");
-};
-module.exports = Comp;
+// const Comp = function (content) {
+//   // 这个content就是传过来的ast语法树
+//   console.log("走到demo-loader了");
+//   return content.replace(/console\.log/g, "");
+// };
+// module.exports = Comp;
 
-// 执行npm link 就把这个包链接到了本地项目上
+// // 执行npm link 就把这个包链接到了本地项目上
+
+const loaderUtils = require("loader-utils");
+// 安装指定版本 loader-utils@1.4.2
+
+module.exports = function (content) {
+  const options = loaderUtils.getOptions(this);
+  console.log(options, "=====options");
+
+  this.callback(null, addSign(content, options.sign));
+
+  return;
+};
+
+function addSign(content, sign) {
+  console.log("我执行到增加标识了");
+  // 每个文件前面都增加一个标识
+  return `/**${sign}*/\n${content}`;
+}
